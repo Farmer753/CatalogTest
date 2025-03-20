802d4cbcd9fe47463fa885f96ac97fa7ace9120c
LL <ludo4ka21@yandex.ru>
2025-03-20 23:30:44 +0300

    progress+error on CategoryProductsScreen

--- app/src/main/java/ru/ll/catalogtest/ui/categoryproducts/CategoryProductsScreen.kt
+++ app/src/main/java/ru/ll/catalogtest/ui/categoryproducts/CategoryProductsScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,15 +18,20 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -43,6 +50,7 @@ import ru.ll.catalogtest.ui.theme.GrayBg
 import ru.ll.catalogtest.ui.theme.GrayDark
 import ru.ll.catalogtest.ui.theme.GrayLite
 import ru.ll.catalogtest.ui.theme.Primary
+import timber.log.Timber
 
 
 @Preview
@@ -69,6 +77,7 @@ fun CategoryProductsPreview() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryProductsScreen(
     categorySlug: String,
@@ -84,13 +93,44 @@ fun CategoryProductsScreen(
             endIcon = null,
             onStartIconClick = onBackClick
         )
+        val errorMessage = viewModel.error.collectAsStateWithLifecycle()
+        val progress = viewModel.progress.collectAsStateWithLifecycle()
+        val textErrorMessage = errorMessage.value
         val products = viewModel.catalog.collectAsStateWithLifecycle()
-        if (products.value != null) {
-            Products(
-                modifier = Modifier.weight(1f),
-                products.value!!
+        PullToRefreshBox(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            isRefreshing = progress.value,
+            onRefresh = viewModel::getData
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
             ) {
-                onProductClick(it)
+                if (products.value != null) {
+                    Products(
+                        modifier = Modifier.weight(1f),
+                        products.value!!
+                    ) {
+                        onProductClick(it)
+                    }
+                }
+                if (textErrorMessage != null) {
+                    Timber.i("textErrorMessage $textErrorMessage")
+
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .background(color = MaterialTheme.colorScheme.background)
+                            .fillMaxWidth(),
+                        text = textErrorMessage,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                }
             }
         }
     }
